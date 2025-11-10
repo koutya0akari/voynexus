7903eb0bce20acd2dbbdb9c3b9dc3dd3458b6d1b
akari0koutya <koutya0akari@gmail.com>
2025-11-10 16:54:38 +0900

    fix:api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 const secretKey = process.env.STRIPE_SECRET_KEY;
 
-export const stripe = secretKey ? new Stripe(secretKey, { apiVersion: "2024-06-20" }) : null;
+export const stripe = secretKey ? new Stripe(secretKey) : null;
 
 export function requireStripe() {
   if (!stripe) {
