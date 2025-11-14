5e8c17ab7aa4655b55b53865a9f96ca7986dd532
akari0koutya <koutya0akari@gmail.com>
2025-11-15 02:18:00 +0900

    edit : icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,8 +43,11 @@ export const metadata: Metadata = {
   },
   manifest: "/manifest.webmanifest",
   icons: {
-    icon: "/icons/icon-192.png",
-    apple: "/icons/icon-192.png",
+    icon: [
+      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
   },
 };
 
